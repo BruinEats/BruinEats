e446ced2862871eb9868840426abdc6bab6bf628
tonyliu1234 <iamtony200110@gmail.com>
2022-02-05 15:57:38 -0800

    models for dininghall and food

---
+++ models/diningHall.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Food = require("./food").FoodSchema;
+
+const DiningHall = new mongoose.Schema({
+    name: {
+        type: String,
+        default: "",
+        required: true,
+        unique: true,
+    },
+
+    food: [Food],
+});
+
+module.exports.DiningHallSchema = DiningHall;
+module.exports.DiningHallModel = mongoose.model("DiningHall", DiningHall);
--- models/food.js
+++ models/food.js
@@ -1,6 +1,24 @@
 const mongoose = require("mongoose");
+const DiningHall = require("./diningHall").DiningHallSchema;
+const Review = require("./review").ReviewSchema;
 
-const Food = new mongoose.Schema({});
+const Food = new mongoose.Schema({
+    name: {
+        type: String,
+        default: "",
+        required: true,
+        unique: true,
+    },
+
+    overallRating: {
+        type: Number,
+        default: 0.0,
+        required: true,
+    },
+
+    reviews: [Review],
+    diningHall: [DiningHall],
+});
 
 module.exports.FoodSchema = Food;
 module.exports.FoodModel = mongoose.model("Food", Food);
