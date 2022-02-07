55029ca3b866e11bff46bf6d917221e9aa0eac10
JeffreytheCoder <y2397371802@gmail.com>
2022-02-07 00:02:16 -0800

    more eslint fix

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,14 +1,14 @@
-const express = require("express");
+const express = require('express')
 const {
   register,
   signin,
   updateUsrInfo,
-} = require("../controllers/userController");
+} = require('../controllers/userController')
 
-const userRouter = express.Router();
+const userRouter = express.Router()
 
-userRouter.post("/register", register);
-userRouter.post("/login", signin);
-userRouter.put("/:_id/update", updateUsrInfo);
+userRouter.post('/register', register)
+userRouter.post('/login', signin)
+userRouter.put('/:_id/update', updateUsrInfo)
 
-module.exports = userRouter;
+module.exports = userRouter
