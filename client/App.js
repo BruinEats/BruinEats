15bb21a960fffe3a31a9644c252a33ebd6734826
JeffreytheCoder <y2397371802@gmail.com>
2022-02-08 03:55:40 -0800

    react native eslint

--- client/App.js
+++ client/App.js
@@ -5,9 +5,7 @@ import tw from 'tailwind-react-native-classnames';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={[tw`text-lg`]}>
-        Open up App.js to start working on your app!
-      </Text>
+      <Text style={[tw`text-lg`]}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
