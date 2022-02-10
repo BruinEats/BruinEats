4808b301c4a4cd24d32f21b5d39e0b10012f6140
JeffreytheCoder <y2397371802@gmail.com>
2022-02-09 16:13:39 -0800

    redux toolkit setup

--- client/App.js
+++ client/App.js
@@ -2,12 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 
+import { Provider } from 'react-redux';
+import { store } from './store';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={[tw`text-lg`]}>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text style={[tw`text-lg`]}>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
