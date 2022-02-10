d37c7720a7670b1d2acb5321d18e0bdca4ade752
JeffreytheCoder <y2397371802@gmail.com>
2022-02-09 17:58:59 -0800

    redux user slice, axios token header, spinner

--- client/App.js
+++ client/App.js
@@ -1,5 +1,7 @@
+import { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import tw from 'tailwind-react-native-classnames';
 import React from 'react';
 
@@ -7,6 +9,11 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadUser } from './slices/userSlice';
+import setAuthToken from './utils/setAuthToken';
+
+import Spinner from './components/global/Spinner';
 
 import SignInScreen from './src/screens/SignInScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
@@ -15,6 +22,17 @@ import HomeScreen from './src/screens/HomeScreen';
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  const dispatch = useDispatch();
+  const { user, loading } = useSelector((state) => state.user);
+
+  useEffect(async () => {
+    const token = await AsyncStorage.getItem('token');
+    if (token) {
+      setAuthToken(token);
+      loadUser(token);
+    }
+  }, []);
+
   return (
     <Provider store={store}>
       <NavigationContainer>
