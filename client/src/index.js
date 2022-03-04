9761d2756bad1166f4ac27291682b9d1aee5c3ab
JeffreytheCoder <y2397371802@gmail.com>
2022-03-03 19:30:57 -0800

    add rnu lib

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
-
+// import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-import Tabs from './Tabs';
+import { ThemeManager } from 'react-native-ui-lib';
 
 import { AuthProvider } from './contexts/AuthContext';
+import Tabs from './Tabs';
+
+ThemeManager.setComponentTheme('Text', {
+  text70: true, // will set the text70 typography modifier prop to be true by default
+  grey20: true, // will set the grey10 color modifier prop to be true by default
+});
 
 const AppNavigation = () => {
   return (
