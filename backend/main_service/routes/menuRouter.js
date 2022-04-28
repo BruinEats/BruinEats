4db638ea6ffaefbfa9713118ced69bbdc1b8f822
yjh020711@ucla.edu <yjh020711@ucla.edu>
2022-04-28 16:39:54 -0700

    nestjs backend init

--- backend/main_service/routes/menuRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getMenu, scrapMenuToday } = require('../controllers/menuController');
-
-const menuRouter = express.Router();
-
-menuRouter.post('/getMenu', getMenu);
-menuRouter.post('/scrapMenuToday', scrapMenuToday);
-
-module.exports = menuRouter;
--- backend/main_service/routes/reviewRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getUsrReview } = require('../controllers/reviewController');
-
-const reviewRouter = express.Router();
-
-reviewRouter.get('/:_id', getUsrReview);
-
-module.exports = reviewRouter;
